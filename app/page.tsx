3f992f5706bda4028a43da0b2cbfe4398eaa9e6e
TMN <t.martin@adsoftware.fr>
2023-12-26 23:09:47 +0100

    tm p

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,122 @@
-import Image from 'next/image'
+"use client";
+
+import { Column, DataGrid, Export } from "devextreme-react/data-grid";
+import "devextreme/dist/css/dx.light.css";
+import { Workbook } from "exceljs";
+import saveAs from "file-saver";
+import { exportDataGrid } from "devextreme/excel_exporter";
+import { Club, fakeJournees } from "./fakes";
+
+type Affluence = {
+  [club in (typeof Clubs)[number]]: number;
+};
+
+const Clubs = [
+  "Stade Brestois 29",
+  "Clermont Foot 63",
+  "Havre AC",
+  "RC Lens",
+  "Lille OSC",
+  "FC Lorient",
+  "Olympique Lyonnais",
+  "Olympique de Marseille",
+  "FC Metz",
+  "AS Monaco FC",
+  "Montpellier Hérault SC",
+  "FC Nantes",
+  "OGC Nice",
+  "Paris Saint-Germain FC",
+  "Stade de Reims",
+  "Stade Rennais FC",
+  "RC Strasbourg Alsace",
+  "Toulouse FC",
+] as const;
 
 export default function Home() {
+  // Function to get the highest and lowest values in each column
+
+  type CellRenderData = {
+    data: any;
+    value: number;
+    column: {
+      dataField: string;
+    };
+  };
+
+  const getHighestAndLowest = (
+    club: Club
+  ): { highest: number; lowest: number } => {
+    const values = fakeJournees.map((j) => j.affluences[club]);
+    const highest = Math.max(...values);
+    const lowest = Math.min(...values);
+    return { highest, lowest };
+  };
+
+  // Custom cell render to apply conditional styling
+  const cellRender = (cellData: CellRenderData) => {
+    const { column, value } = cellData;
+    const club = column.dataField.split(".")[1];
+    const { highest, lowest } = getHighestAndLowest(club);
+    let backgroundColor = "#FFFFFF"; // Default background
+
+    if (value === highest) {
+      backgroundColor = "#00FF00"; // Green for the highest value
+    } else if (value === lowest) {
+      backgroundColor = "#FF0000"; // Red for the lowest value
+    }
+
+    return (
+      <div style={{ backgroundColor, width: "100%", height: "100%" }}>
+        {value}
+      </div>
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <DataGrid<(typeof fakeJournees)[0]>
+      dataSource={fakeJournees}
+      onExporting={(e) => {
+        const workbook = new Workbook();
+        const worksheet = workbook.addWorksheet("Main sheet");
+        exportDataGrid({
+          component: e.component,
+          worksheet: worksheet,
+          customizeCell: function (options) {
+            options.excelCell.font = { name: "Arial", size: 12 };
+            options.excelCell.alignment = { horizontal: "left" };
+          },
+        }).then(function () {
+          workbook.xlsx.writeBuffer().then(function (buffer) {
+            saveAs(
+              new Blob([buffer], { type: "application/octet-stream" }),
+              "DataGrid.xlsx"
+            );
+          });
+        });
+      }}
+    >
+      {" "}
+      <Column caption="Journee" dataField="number" />
+      {Clubs.map((club) => (
+        <Column
+          key={club}
+          caption={club}
+          dataField={`affluences.${club}`}
+          cellRender={cellRender}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      ))}
+      <Column
+        caption="Total"
+        allowSorting
+        calculateCellValue={(data) => {
+          let total = 0;
+          for (const club of Clubs) {
+            total += data.affluences[club];
+          }
+          return total;
+        }}
+      />
+      <Export enabled={true} />
+    </DataGrid>
+  );
 }
