1dc8009dbcbd7ef4f0299425ac1c22208c6e48a9
TMN <t.martin@adsoftware.fr>
2023-12-27 20:46:31 +0100

    pnpm

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,6 @@ import { crawl } from "./crawler/basic";
 
 export default async function Home() {
   const journees = await crawl();
+  if (!journees) return <p>Chargement...</p>;
   return <Datagrid journees={journees} />;
 }
